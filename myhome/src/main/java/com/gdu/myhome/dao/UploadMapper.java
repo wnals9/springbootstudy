78cad2349b138c8db386eaf2f79176953d236854
goodeeit <gt_min@naver.com>
2023-11-21 16:57:16 +0900

    20231121

---
+++ myhome/src/main/java/com/gdu/myhome/dao/UploadMapper.java
@@ -0,0 +1,24 @@
+package com.gdu.myhome.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.gdu.myhome.dto.AttachDto;
+import com.gdu.myhome.dto.UploadDto;
+
+@Mapper
+public interface UploadMapper {
+  public int insertUpload(UploadDto upload);
+  public int insertAttach(AttachDto attach);
+  public int getUploadCount();
+  public List<UploadDto> getUploadList(Map<String, Object> map);
+  public UploadDto getUpload(int uploadNo);
+  public List<AttachDto> getAttachList(int uploadNo);
+  public AttachDto getAttach(int attachNo);
+  public int updateDownloadCount(int attachNo);
+  public int updateUpload(UploadDto upload);
+  public int deleteAttach(int attachNo);
+  public int deleteUpload(int uploadNo);
+}
